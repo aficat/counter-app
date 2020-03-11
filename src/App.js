d60c4a944bea17d2b7de6f95f5bc93980bca7be5
aficat <aficatrashid@gmail.com>
2020-03-11 11:45:30 +0800

    counter app with react testing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+import { Jumbotron } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
+
+  makeIncrementer = amount => () =>
+    this.setState(prevState => ({
+      count: prevState.count + amount,
+    }));
+
+  increment = this.makeIncrementer(1);
+
+  decrement = this.makeIncrementer(-1);
+
+  render() {
+    return (
+      <div className="container" style={{ paddingTop: 50 }}>
+        <Jumbotron>
+          <h4>Count: {this.state.count}</h4>
+          <br />
+          <Button className="increment" onClick={this.increment}>Increment</Button>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Button className="decrement" onClick={this.decrement}>Decrement</Button>
+        </Jumbotron>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,50 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App component', () => {
+  it('starts with a count of 0', () => {
+    const wrapper = shallow(<App />);
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: 0');
+  });
+
+  it('increments count by 1 when the increment button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const incrementBtn = wrapper.find('button.increment');
+    incrementBtn.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: 1');
+  });
+
+  it('decrements count by 1 when the decrement button is clicked', () => {
+    const wrapper = shallow(<App />);
+    const decrementBtn = wrapper.find('button.decrement');
+    decrementBtn.simulate('click');
+    const text = wrapper.find('p').text();
+    expect(text).toEqual('Count: -1');
+  });
+});
+
+describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  test('knows that 2 and 2 make 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
+
+it('matches the snapshot', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
