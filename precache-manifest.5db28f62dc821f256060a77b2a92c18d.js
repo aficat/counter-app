bebe2c92118172e84cefac0d524ae14542a885d0
aficat <aficatrashid@gmail.com>
2020-05-06 20:56:38 +0800

    Updates

---
+++ precache-manifest.5db28f62dc821f256060a77b2a92c18d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543c53e71d561a265e415a797b0655b4",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "f830417201002945f265",
+    "url": "/counter-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fd969ef9f16f3d81a907",
+    "url": "/counter-app/static/js/2.e613388e.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/counter-app/static/js/2.e613388e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f830417201002945f265",
+    "url": "/counter-app/static/js/main.8840112a.chunk.js"
+  },
+  {
+    "revision": "9bd1b149175f6f6a7809",
+    "url": "/counter-app/static/js/runtime-main.8d74ac5e.js"
+  }
+]);
